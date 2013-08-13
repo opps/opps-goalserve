e3fccb6102d4697c043a9f4ae914ad118d2c0d16
Bruno Rocha <rochacbruno@gmail.com>
2013-08-13 06:56:34 -0300

    JSON cleaned

--- opps/goalserve/views.py
+++ opps/goalserve/views.py
@@ -40,7 +40,7 @@ def serialize_date(dt):
 def serialize(data, exclude=None):
     exclude = exclude or []
 
-    exclude += ['created_at', 'image_base']
+    exclude += ['created_at', 'image_base', 'g_bet_id']
 
     new_data = {k:v for k, v in data.iteritems()
                 if not k.startswith('_') and
@@ -66,16 +66,42 @@ def response_mimetype(request):
     return "text/plain"
 
 def get_players(_players):
+
+# status: "player",
+# birthplace: "Monte Azul Paulista",
+# number: null,
+# g_fix_id: null,
+# weight: null,
+# age: 28,
+# g_event_id: null,
+# updated_at: "2013-08-13T09:21:42.354054Z",
+# birthdate: "1985-04-04Z",
+# g_static_id: null,
+# g_id: "37269",
+# team_id: 10,
+# image: "http://placehold.it/50x50/",
+# nationality: "Brazil",
+# position: "D",
+# height: null,
+# g_player_id: "37269",
+# id: 1302,
+# name: "Rodolfo"
+
     players = []
     for _player in _players:
-        player = serialize(_player.player.__dict__)
+        player = serialize(
+            _player.player.__dict__,
+            exclude=['birthdate', 'birthplace', 'g_static_id', 'g_id', 'height', 'weight',
+                     'team_id', 'nationality', 'updated_at', 'age', 'g_event_id']
+        )
 
         player['number'] = _player.player_number
-        player['status'] = _player.player_status
-        player['position'] = _player.player_position or player['position']
+        # player['status'] = _player.player_status
+        # player['position'] = _player.player_position or player['position']
         player['image'] = _player.player.image_url
 
-        players.append(player)
+        if player.get('name'):
+            players.append(player)
     return players
 
 def get_team_stats(_stats):
@@ -128,6 +154,18 @@ def get_event(_event):
 
     return _event.__dict__
 
+
+# {"round_standings": {
+#         "round": "",
+#         "date": "",
+#         "teams": [
+#             {
+#                 "name": "",
+#                 "position": "",
+#                 "points": "",
+#                 "games": ""},
+#         ]}}
+
 def get_standings(_category):
     standings = MatchStandings.objects.filter(category=_category).order_by('position')
     if not standings:
@@ -145,9 +183,13 @@ def get_standings(_category):
             serialize(
                 stand.__dict__,
                 exclude=['id', 'g_player_id', 'g_fix_id', 'g_bet_id',
-                         'category_id', 'g_event_id', 'country_id', 'g_id']
+                         'category_id', 'g_event_id', 'country_id', 'g_id',
+                         'status', 'overall_w', 'updated_at', 'overall_gs',
+                         'g_static_id', 'team_id', 'total_gd', 'recent_form', 'timestamp',
+                         'overall_ga', 'overall_l']
             )
         )
+        rounds[stand.round] = sorted(rounds[stand.round], key=lambda team: int(team['position']))
     return rounds
 
 
@@ -162,60 +204,72 @@ def match(request, match_pk, mode='response'):
     _match = get_object_or_404(Match, pk=int(match_pk))
     data = serialize(
         _match.__dict__,
-        exclude=['time_name', 'attendance_name', 'referee_name', 'ht_result']
+        exclude=['time_name', 'attendance_name', 'referee_name', 'ht_result', 'g_player_id',
+                 'g_event_id', 'visitorteam_goals', 'localteam_goals', 'localteam_id',
+                 'visitorteam_id', 'stadium_id', 'category_id']
 
     )
     data['result'] = {'localteam': _match.localteam_goals, 'visitorteam': _match.visitorteam_goals}
     data['stadium'] = serialize(get_dict(_match, 'stadium'))
     if data['stadium']:
-        data['stadium']['image'] = _match.stadium.image_url
+        # data['stadium']['image'] = _match.stadium.image_url
+        data['stadium'] = {"name": data['stadium'].get('name')}
     data['tournament'] = _match.category.name
     data['country'] = _match.category.country.name.title()
 
-    data['comments'] = [
-        serialize(
-            comment.__dict__,
-            exclude=[
-                'match_id', 'g_bet_id', 'g_event_id',
-                'g_static_id', 'g_player_id', 'g_fix_id', 'id'
-            ]
-        ) for comment in _match.matchcommentary_set.all()
-    ]
+    # data['comments'] = [
+    #     serialize(
+    #         comment.__dict__,
+    #         exclude=[
+    #             'match_id', 'g_bet_id', 'g_event_id',
+    #             'g_static_id', 'g_player_id', 'g_fix_id', 'id'
+    #         ]
+    #     ) for comment in _match.matchcommentary_set.all()
+    # ]
 
     data['events'] = [
         serialize(
             get_event(event),
             exclude=[
                 'match_id', 'g_bet_id',
-                'g_static_id', 'g_player_id', 'g_fix_id', 'id', 'g_id'
+                'g_static_id', 'g_player_id', 'g_fix_id', 'id', 'g_id', 'own_goal', 'updated_at',
+                'penalty', 'result', 'player_id', 'team_id'
             ]
         ) for event in _match.matchevent_set.all()
     ]
 
     # LOCALTEAM
-    data['localteam'] = serialize(get_dict(_match, 'localteam'))
+    data['localteam'] = serialize(
+        get_dict(_match, 'localteam'),
+        exclude=['g_fix_id', 'country_id', 'g_event_id', 'updated_at', 'g_static_id',
+                 'founded', 'full_name', 'stadium_id', 'g_player_id']
+    )
     data['localteam']['players'] = get_players(
         _match.localteam.matchlineup_set.filter(match=_match).order_by('player_status', 'player_number')
     )
-    data['localteam']['stats'] = get_team_stats(
-        _match.matchstats_set.filter(team=_match.localteam)
-    )
-    data['localteam']['substitutions'] = get_team_substitutions(
-        _match.matchsubstitutions_set.filter(team=_match.localteam)
-    )
+    # data['localteam']['stats'] = get_team_stats(
+    #     _match.matchstats_set.filter(team=_match.localteam)
+    # )
+    # data['localteam']['substitutions'] = get_team_substitutions(
+    #     _match.matchsubstitutions_set.filter(team=_match.localteam)
+    # )
     data['localteam']['image'] = _match.localteam.image_url
 
     # VISITORTEAM
-    data['visitorteam'] = serialize(get_dict(_match, 'visitorteam'))
+    data['visitorteam'] = serialize(
+        get_dict(_match, 'visitorteam'),
+        exclude=['g_fix_id', 'country_id', 'g_event_id', 'updated_at', 'g_static_id',
+                 'founded', 'full_name', 'stadium_id', 'g_player_id']
+    )
     data['visitorteam']['players'] = get_players(
         _match.visitorteam.matchlineup_set.filter(match=_match).order_by('player_status', 'player_number')
     )
-    data['visitorteam']['stats'] = get_team_stats(
-        _match.matchstats_set.filter(team=_match.visitorteam)
-    )
-    data['visitorteam']['substitutions'] = get_team_substitutions(
-        _match.matchsubstitutions_set.filter(team=_match.visitorteam)
-    )
+    # data['visitorteam']['stats'] = get_team_stats(
+    #     _match.matchstats_set.filter(team=_match.visitorteam)
+    # )
+    # data['visitorteam']['substitutions'] = get_team_substitutions(
+    #     _match.matchsubstitutions_set.filter(team=_match.visitorteam)
+    # )
     data['visitorteam']['image'] = _match.visitorteam.image_url
 
     data['round_standings'] = get_standings(_match.category)
