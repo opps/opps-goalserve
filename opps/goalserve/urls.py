4d26fb82fcb1ab037f3b03163837b37f72a47a03
Bruno Rocha <rochacbruno@gmail.com>
2013-08-16 01:36:14 -0300

    Fix Pep8 in urls.py

--- opps/goalserve/urls.py
+++ opps/goalserve/urls.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns
-from .views import match, ajax_categories_by_country_name, \
-                   ajax_match_by_category_id, ajax_get_matches, get_task_status
+from opps.goalserve.views import (match, ajax_categories_by_country_name,
+                                  ajax_match_by_category_id, ajax_get_matches, get_task_status)
 
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns('',
     (r'^match/(?P<match_pk>\d+)/$', match, {}, 'match'),
-    (r'^ajax_categories_by_country_name/(?P<country_name>\w+)/$', ajax_categories_by_country_name, {}, 'ajax_categories_by_country_name'),
-    (r'^ajax_match_by_category_id/(?P<category_id>\d+)/$', ajax_match_by_category_id, {}, 'ajax_match_by_category_id'),
+    (r'^ajax_categories_by_country_name/(?P<country_name>\w+)/$',
+        ajax_categories_by_country_name, {}, 'ajax_categories_by_country_name'),
+    (r'^ajax_match_by_category_id/(?P<category_id>\d+)/$',
+        ajax_match_by_category_id, {}, 'ajax_match_by_category_id'),
     (r'^ajax_get_matches/(?P<country_name>\w+)/$', ajax_get_matches, {}, 'ajax_get_matches'),
-    (r'^ajax_get_matches/(?P<country_name>\w+)/(?P<match_id>\d+)/$', ajax_get_matches, {}, 'ajax_get_matches_id'),
-    (r'^get_task_status/(?P<task_id>[\w-]+)/$', get_task_status, {}, 'get_task_status'),
-)
+    (r'^ajax_get_matches/(?P<country_name>\w+)/(?P<match_id>\d+)/$',
+        ajax_get_matches, {}, 'ajax_get_matches_id'),
+    (r'^get_task_status/(?P<task_id>[\w-]+)/$', get_task_status, {}, 'get_task_status'))
