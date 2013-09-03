26746b39ab49cc9c517a0d276cbfacb17d3db379
Bruno Rocha <rochacbruno@gmail.com>
2013-09-03 03:08:40 -0300

    Added REST API for Players, needed mimeparse and django-tastypie

--- opps/goalserve/api.py
+++ opps/goalserve/api.py
@@ -2,6 +2,64 @@
 
 from opps.goalserve.models import Team, Player, F1Team, Driver
 
+# tastypie
+from tastypie.authorization import Authorization
+from tastypie.resources import ModelResource
+from tastypie.exceptions import Unauthorized
+
+
+def check_access(user):
+    if not (user.is_authenticated() and (user.is_superuser or user.is_staff)):
+        raise Unauthorized("Sorry, access denied.")
+
+class GoalserveAuthorization(Authorization):
+
+
+    def read_list(self, object_list, bundle):
+        # This assumes a ``QuerySet`` from ``ModelResource``.
+        check_access(bundle.request.user)
+        return object_list
+
+    def read_detail(self, object_list, bundle):
+        # Is the requested object owned by the user?
+        check_access(bundle.request.user)
+        return True
+
+    def create_list(self, object_list, bundle):
+        # Assuming their auto-assigned to ``user``.
+        check_access(bundle.request.user)
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        check_access(bundle.request.user)
+        return True
+
+    def update_list(self, object_list, bundle):
+        check_access(bundle.request.user)
+        return object_list
+
+    def update_detail(self, object_list, bundle):
+        check_access(bundle.request.user)
+        return True
+
+    def delete_list(self, object_list, bundle):
+        # Sorry user, no deletes for you!
+        raise Unauthorized("Sorry, no deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.")
+
+class PlayerResource(ModelResource):
+    class Meta:
+        queryset = Player.objects.all()
+        resource_name = "player"
+        authorization = GoalserveAuthorization()  # Authorization
+        excludes = ('image_base', 'created_at', 'extra', 'g_bet_id',
+                    'g_driver_id', 'g_event_id', 'g_fix_id', 'g_player_id',
+                    'g_static_id', 'g_team_id')
+
+# home made api for tasks
+
 def get_team_by_id(_id):
     try:
         return Team.objects.get(pk=int(_id))
--- opps/goalserve/urls.py
+++ opps/goalserve/urls.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns
+from django.conf.urls import patterns, include
 from opps.goalserve.views import (match, ajax_categories_by_country_name,
                                   ajax_match_by_category_id, ajax_get_matches, get_task_status)
+
+from tastypie.api import Api
+from opps.goalserve.api import PlayerResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(PlayerResource())
 
 urlpatterns = patterns('',
     (r'^match/(?P<match_pk>\d+)/$', match, {}, 'match'),
@@ -12,4 +18,6 @@ urlpatterns = patterns('',
     (r'^ajax_get_matches/(?P<country_name>\w+)/$', ajax_get_matches, {}, 'ajax_get_matches'),
     (r'^ajax_get_matches/(?P<country_name>\w+)/(?P<match_id>\d+)/$',
         ajax_get_matches, {}, 'ajax_get_matches_id'),
-    (r'^get_task_status/(?P<task_id>[\w-]+)/$', get_task_status, {}, 'get_task_status'))
+    (r'^get_task_status/(?P<task_id>[\w-]+)/$', get_task_status, {}, 'get_task_status'),
+    (r'^api/', include(v1_api.urls)),
+)
