ac1f07625060d584616dd162688afa33a47a2eb0
Guilherme Gondim <guilherme@semente.io>
2014-02-17 12:58:39 -0300

    Add "southamerica"

--- opps/goalserve/utils.py
+++ opps/goalserve/utils.py
@@ -206,7 +206,9 @@ def get_tournament_standings(**kwargs):
             return {}
 
     category_kwargs =  filter_kwargs_by_key('category__')
-    categories = Category.objects.filter(country__name__in=['brazil', 'intl', 'international'], **category_kwargs)
+    categories = Category.objects.filter(country__name__in=[
+        'brazil', 'intl', 'international', 'southamerica'
+    ], **category_kwargs)
 
     for category in categories:
         item = {'title': category.name, 'id': category.id, 'slug': slugify(category.name), 'display_name': category.display_name}
